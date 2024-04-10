4c59153906c6ebdfa74632904f0f880c1ea43283
Dereje-boy <Derejeg35@gmail.com>
2024-04-10 10:51:42 +0300

    UI enhanced to work with movie selection and deselection too

--- public/script/index.js
+++ public/script/index.js
@@ -26,6 +26,7 @@ thumbnail_input.addEventListener('change',function (e){
 console.log($('.single-card-container'));
 Array.from($('.single-card-container')).forEach(singleCardContainer=>{
     singleCardContainer.addEventListener('click',(e)=>{
+        console.log(e.target.id);
         const thisID = singleCardContainer.id;
         movieClicked(thisID)
         console.log(singleCardContainer.id)
@@ -39,7 +40,8 @@ function movieClicked(containerID) {
             $(`#${svgContainerID}`).css('top','-350px')
             $(`#${svgContainerID} #ok-svg`).css('scale','1')
             $(`#${svgContainerID} #ok-right`).css('fill','green')
-            selectedMovies.pop(containerID);
+            selectedMovies.splice(selectedMovies.indexOf(containerID),1);
+            renderSelectedMovies();
         }
     else// perform selection
         {
@@ -47,7 +49,36 @@ function movieClicked(containerID) {
             $(`#${svgContainerID} #ok-svg`).css('scale','.4')
             $(`#${svgContainerID} #ok-right`).css('fill','white')
             selectedMovies.push(containerID);
+            renderSelectedMovies();
         }
 }
 
 const selectedMovies = [];
+
+function renderSelectedMovies() {
+    $('#movie-modal-body').empty();
+    selectedMovies.forEach((movie,index)=>{
+
+        // const thisMovieP = $(`<p class="align-items-center d-flex justify-content-center">${movie}</p>`);
+
+        const movie_name_h2 = $('#'+movie).children('.text-and-buttons').children('.movie-name')
+        console.log(movie_name_h2.text());
+
+        const containerDiv = $('<div class="d-flex justify-content-between align-items-center overflow-hidden"></div>')
+        const numberP = $(`<p class="px-2 fw-bold text-danger overflow-hidden">${index+1}</p>`)
+
+        const thisMovieP = $(`<p class="fw-bold flex-grow-1 text-success d-flex justify-content-start">${movie_name_h2.text()}</p>`);
+
+        const deleteMovie = $(`<button class="btn btn-danger fw-bold ">X</button>`);
+
+        $(deleteMovie).on('click',(e)=>{
+            console.log($('#' + movie).click());
+        })
+
+        $(containerDiv).append($(numberP))
+        $(containerDiv).append($(thisMovieP))
+        $(containerDiv).append($(deleteMovie))
+
+        $('#movie-modal-body').append(containerDiv);
+    })
+}
